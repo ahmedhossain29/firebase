51ce263733ce65c200b155835c4254cf39ed6d36
AHMED <hossainahmad76@gmail.com>
2023-12-16 21:37:32 +0600

    Firebase Get data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'detailsPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,35 +16,84 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // Future<void> initializeDefault() async {
-  //   FirebaseApp app = await Firebase.initializeApp();
-  // }
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: StudentList(),
+      home: MatchList(),
     );
   }
 }
 
-class StudentList extends StatelessWidget {
-  const StudentList({super.key});
+class MatchList extends StatefulWidget {
+  const MatchList({super.key});
+
+  @override
+  State<MatchList> createState() => _MatchListState();
+}
+
+class _MatchListState extends State<MatchList> {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  List<Football> footballList = [];
+
+  Future<void> getfootballMatch() async {
+    final QuerySnapshot result =
+        await firebaseFirestore.collection('football').get();
+    for (QueryDocumentSnapshot element in result.docs) {
+      Football football = Football(element.get('teamName'),
+          element.get('goals'), element.get('time'), element.get('totalTime'));
+      footballList.add(football);
+    }
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getfootballMatch();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Student List'),
+        title: const Text('Match List'),
       ),
       body: ListView.builder(
-          itemCount: 5,
+          itemCount: footballList.length,
           itemBuilder: (context, index) {
-            return const ListTile(
-              title: Text('Hossain'),
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DetailsPage(
+                                  matchList: footballList[index],
+                                )));
+                  },
+                  title: Text(
+                    footballList[index].name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  trailing: const Icon(Icons.arrow_forward),
+                ),
+              ),
             );
           }),
     );
   }
+}
+
+class Football {
+  final String name;
+  final String goal;
+  final String time;
+  final String totalTime;
+
+  Football(this.name, this.goal, this.time, this.totalTime);
 }
